f04ebee5db828ff30b0af68f1a217b1a1d044c46
Marc Schöchlin <marc.schoechlin@flipapp.de>
2020-12-12 22:39:21 +0100

    add new kubefwd

--- manifests/profile/software.pp
+++ manifests/profile/software.pp
@@ -11,7 +11,7 @@
 class ubuntudesktop::profile::software (
   Array[String] $packages_additional = [],
   Array[String] $packages_exclude    = [],
-  Array[String] $ide_snaps           = ["intellij-idea-community", "pycharm-community", "gradle", "gitkraken", "code"],
+  Array[String] $ide_snaps           = ["intellij-idea-community", "pycharm-community", "gradle", "gitkraken", "code", "dbeaver-ce" ],
   Boolean $nextcloud                 = true,
   Boolean $virtualbox                = true,
   String $virtualbox_version         = "6.1",
@@ -327,7 +327,7 @@ allow /usr/bin/vim.gtk3 ixr,
       extra_args => "--classic"
     }
     ubuntudesktop::deb_package_install_from_url { "kubefwd":
-      uri => "https://github.com/txn2/kubefwd/releases/download/1.14.7/kubefwd_1.14.7_linux_amd64.deb"
+      uri => "https://github.com/txn2/kubefwd/releases/download/1.17.3/kubefwd_amd64.deb"
     }
     file { '/etc/sudoers.d/kubefwd':
     owner   => 'root',
